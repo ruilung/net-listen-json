3fe1b8e7a0958c7719157645cc523a1ca7510c09
ruilung <ruilung@gmail.com>
2019-01-02 17:57:09 +0800

    add exclude list in parameter

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # functions
 1. get listen ports from os.
 2. make it to zabbix low level discovery format (json).
+3. exclude list in parameter i.g nlj.py 1-2000
 
 # output example
-{"data": ["{#PORT}":"22", "{#PORT}":"135", "{#PORT}":"445", "{#PORT}":"1801", "{#PORT}":"2103", "{#PORT}":"2105", "{#PORT}":"2107", "{#PORT}":"5357", "{#PORT}":"7680", "{#PORT}":"34910", "{#PORT}":"49664", "{#PORT}":"49665", "{#PORT}":"49666", "{#PORT}":"49667", "{#PORT}":"49668", "{#PORT}":"49670", "{#PORT}":"49789", "{#PORT}":"50123", "{#PORT}":"49828", "{#PORT}":"139"]}
+{"data": ["{#PORT}":"2103", "{#PORT}":"2105", "{#PORT}":"2107", "{#PORT}":"5357", "{#PORT}":"7680", "{#PORT}":"34910", "{#PORT}":"49664", "{#PORT}":"49665", "{#PORT}":"49666", "{#PORT}":"49667", "{#PORT}":"49668", "{#PORT}":"49670", "{#PORT}":"49789", "{#PORT}":"50123", "{#PORT}":"49828", "{#PORT}":"139"]}
--- nlj.py
+++ nlj.py
@@ -1,8 +1,15 @@
 import json
 import platform
 import os
+import sys
 
 def get_netlisten():
+    exclude_list=[]
+    if len(sys.argv) > 1:
+        for i in sys.argv[1:]:
+            exclude_list.append(i.split('-'))
+        #print exclude_list
+
     system=platform.system()
     lst=[]
     rlst=[]
@@ -11,7 +18,14 @@ def get_netlisten():
         lst=os.popen("ss -tln | awk '{print $4}' | grep -v '^:' | awk -F: '{print $2}' | grep -v '^$'")
         for i in lst:
             str=('{"{#PORT}":"'+i+'"}').replace('\n','')
-            rlst.append(str)
+            append_flag = 1
+            for e in exclude_list:
+                # print e[0], e[1]
+                if int(e[0]) <= int(i) <= int(e[1]):
+                    append_flag = 0
+            if append_flag:
+                rlst.append(str)
+
 
     if system=='Windows':
         lst=os.popen('netstat -an | find /v "[::]" | findstr LISTEN')
@@ -19,12 +33,20 @@ def get_netlisten():
         for i in lst:
             j=i.split()[1]
             k=j.split(':')[1]
-            rlst.append('{"{#PORT}":"'+k+'"}')
+            append_flag=1
+            for e in exclude_list:
+                #print e[0], e[1]
+                if int(e[0]) <= int(k) <= int(e[1]):
+                    append_flag=0
+            if append_flag:
+                rlst.append('{"{#PORT}":"'+k+'"}')
+
     return rlst
 
 
 
 if __name__ == '__main__':
+
 
     print json.dumps({ "data":get_netlisten() }).replace('\\"','"').replace('"{"','{"').replace('"}"','"}')
     #print json.dumps({"data": get_netlisten()}).replace('\\"', "\"").replace('\"\"', "\"")
