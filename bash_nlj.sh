d2c4303268a680f55ca650aa00d7b0e3bdac5542
ruilung <ruilung@users.noreply.github.com>
2020-05-25 16:55:29 +0800

    update bash_nlj.sh , change "::1" to "::"
    if don't do this. will get an error port 1

--- bash_nlj.sh
+++ bash_nlj.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LISTENPORT=$(ss -tln | awk '{print $4}' | grep -Eo ':[0-9]+' |  awk -F: '{print $2}' | sort | uniq)
+LISTENPORT=$(ss -tln | awk '{print $4}'  | sed 's|::1|::|g' | grep -Eo ':[0-9]+' |  awk -F: '{print $2}' | sort -n | uniq)
 echo -n '{"data":['
 for port in $LISTENPORT; do
         echo -n '{"{#PORT}":"'${port}'"},'
