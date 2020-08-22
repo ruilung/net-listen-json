34a1cf5be75375ff74959ef5940dcf9724f11690
ruilung <ruilung@users.noreply.github.com>
2020-08-22 22:06:15 +0800

    Update bash_nlj.sh

--- bash_nlj.sh
+++ bash_nlj.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LISTENPORT=$(ss -tln | awk '{print $4}'  | sed 's|::1|::|g' | grep -Eo ':[0-9]+' |  awk -F: '{print $2}' | sort -n | uniq)
+LISTENPORT=$(ss -tln | awk '{print $4}' | sed 's|::1|::|g' | sed 's|::ffff:||g' | grep -Eo ':[0-9]+' | awk -F: '{print $2}' | sort -n | uniq)
 echo -n '{"data":['
 for port in $LISTENPORT; do
         echo -n '{"{#PORT}":"'${port}'"},'
